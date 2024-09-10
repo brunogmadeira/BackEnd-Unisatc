1f782e70c7891ae29c3d0b376733ce9dde00338d
Bruno Girardi Madeira <girardi791@gmail.com>
2024-09-10 20:34:25 -0300

    exercicio07

---
+++ exercicio07/src/main/java/com/example/exercicio07/Exercicio07Application.java
@@ -0,0 +1,40 @@
+package com.example.exercicio07;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+
+
+@SpringBootApplication
+@RestController
+public class Exercicio07Application {
+
+        @GetMapping("/")
+        public String nomezinnhos(@RequestParam String firstname, @RequestParam String secondName) throws Exception {
+            while(true) {
+                String[] vetorElementos = new String[4];
+                vetorElementos[0] = "Agua";
+                vetorElementos[1] = "Fogo";
+                vetorElementos[2] = "Ar";
+                vetorElementos[3] = "Terra";
+                if(firstname == "") return "";
+                Random geradorAleatorio = new Random();
+                Integer numeroAleatorio = geradorAleatorio.nextInt();
+                Integer indiceAleatorio = geradorAleatorio.nextInt(4);
+                String elemento = vetorElementos[indiceAleatorio];
+
+                System.out.println(elemento + firstname + secondName + numeroAleatorio);
+            }
+        }
+    }
+
+
+
+
+
+
+
